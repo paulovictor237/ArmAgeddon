2323e4919689e9278c6a0a47e8b0575c105cf816
peve <paulovictor237@gmail.com>
2020-08-16 20:18:57 -0300

    5DOF Aplicações

---
+++ ArmAgeddonRobot/src/test_custom1.cpp
@@ -0,0 +1,120 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013, SRI International
+ *  All rights reserved.
+ *  Copyright (c) 2017, Jonathan Cacace
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of SRI International nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Authors: Sachin Chitta
+						Jonathan Cacace
+*/
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/LinearMath/Quaternion.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  // iniciacao padrao para nodes
+  ros::init(argc, argv, "move_group_interface_tutorial");
+  ros::NodeHandle node_handle;
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+  /* This sleep is ONLY to allow Rviz to come up */
+  sleep(2.0);
+
+  // nome do grupo que sera movimentado
+  static const std::string PLANNING_GROUP = "robot_arm";
+  // linhas padrao para criar um grupo a ser movimentado
+  moveit::planning_interface::MoveGroupInterface group(PLANNING_GROUP);
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  const robot_state::JointModelGroup* joint_model_group = group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+  // aqui inicia o programa de fato
+  ROS_INFO("INICIANDO PACOTES");
+  // The package MoveItVisualTools
+  namespace rvt = rviz_visual_tools;
+  moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
+  visual_tools.deleteAllMarkers();
+  // inicia o controle por botoes
+  visual_tools.loadRemoteControl();
+  // pacotes para a interface - text, cylinders, and spheres
+  Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+  text_pose.translation().z() = 1.6;
+  visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
+
+  // (Optional) Create a publisher for visualizing plans in Rviz.
+  ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+  moveit_msgs::DisplayTrajectory display_trajectory;
+
+  ROS_INFO("Movimento para HOME");
+  //seta o movimento
+  // group.setRandomTarget();
+  moveit::core::RobotStatePtr current_state = group.getCurrentState();
+  std::vector<double> joint_group_positions;
+  current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+  cout << "quantidade de juntas: " << joint_group_positions.size() << endl;
+  joint_group_positions[0] = 0.0;  // radians
+  joint_group_positions[1] = 0.0;  // radians
+  joint_group_positions[2] = 0.0;  // radians
+  joint_group_positions[3] = 0.0;  // radians
+  joint_group_positions[4] = 0.0;  // radians
+  joint_group_positions[5] = 0.0;  // radians
+  group.setJointValueTarget(joint_group_positions);
+  ROS_INFO("GetPlanningFrame: %s", group.getPlanningFrame().c_str());
+  ROS_INFO("GetEndEffectorLink: %s", group.getEndEffectorLink().c_str());
+  std::vector<std::string> nomes;
+  nomes = group.getJointNames();
+  for (auto &valor : nomes) ROS_INFO("JointNames: %s ", valor.c_str());
+  nomes = group.getLinkNames();
+  for (auto &valor : nomes) ROS_INFO("Links: %s ", valor.c_str());
+  //move o objeto
+  group.move();
+  // Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations
+  visual_tools.trigger();
+  // prompt trava o programa esperando o proximo passo
+  visual_tools.prompt("Pressione 'next'no RvizVisualToolsGui para continuar");
+
+  ros::shutdown();
+
+ return 0;
+}
