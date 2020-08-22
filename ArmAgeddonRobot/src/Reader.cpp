7027ee6f8dfa8108a0efa279f44eb68823572ee9
peve <paulovictor237@gmail.com>
2020-08-22 19:28:20 -0300

    Reader

--- ArmAgeddonRobot/src/Reader.cpp
+++ ArmAgeddonRobot/src/Reader.cpp
@@ -30,12 +30,8 @@ using namespace std;
 
 void outputLine( double x,double y,double z,double rx,double ry,double rz )
 {
-  //  cout.precision(17);
-  //  std::cout.precision(2);
-  //  std::cout.setf(std::ios::fixed);
-  //  cout << setprecision(3) << fixed;
   cout << std::fixed << std::setprecision(2);
-  cout << "outputLine :"
+  cout << "FileLine>> "
        << "x: "  << x << setw( 8 ) 
        << "y: "  << y << setw( 8 )
        << "z: "  << z << setw( 8 )
@@ -43,60 +39,73 @@ void outputLine( double x,double y,double z,double rx,double ry,double rz )
        << "ry: " << ry << setw( 8 )
        << "rz: " << rz  
        << endl;
-} // end function outputLine
+  return;
+}
 
-int main()
-{
-//+-------------------------------------------------------------------------------+
-  // Obter o endereco relativo do pacote ArmAgeddonRobot
-  string RelativePath=ros::package::getPath("ArmAgeddonRobot");
-  cout << RelativePath << endl;
-//+-------------------------------------------------------------------------------+
-  // Informar o numero de linhas do arquivo 
-  std::ifstream inFile(RelativePath + "/arquivos/test2.txt"); 
-  if(!inFile) {
-    cout << "Cannot open input file.\n";
-    return 1;
-  }
-  cout << "Numero de linhas: " << std::count(std::istreambuf_iterator<char>(inFile),std::istreambuf_iterator<char>(), '\n')+1 << endl;
-  inFile.close();
-//+-------------------------------------------------------------------------------+
-  // ler o arquivo 
-  ifstream inClientFile(RelativePath + "/arquivos/test2.txt");
+
+void ExtractPoints(std::ifstream &inClientFile ,std::vector<geometry_msgs::Pose> &waypoints){
+  geometry_msgs::Pose target_pose;
+  tf2::Quaternion rpy2quaternion;
+
   double x,y,z,rx,ry,rz;
   string descartar;
   while (!inClientFile.eof()){
-    inClientFile >> descartar;
-    inClientFile >> x ;
-    inClientFile >> descartar;
-    inClientFile >> y ;
-    inClientFile >> descartar;
-    inClientFile >> z ;
-    inClientFile >> descartar;
-    inClientFile >> rx;
-    inClientFile >> descartar;
-    inClientFile >> ry;
-    inClientFile >> descartar;
-    inClientFile >> rz;
+    inClientFile >> descartar >> x ;
+    inClientFile >> descartar >> y ;
+    inClientFile >> descartar >> z ;
+    inClientFile >> descartar >> rx;
+    inClientFile >> descartar >> ry;
+    inClientFile >> descartar >> rz;
     outputLine(x,y,z,rx,ry,rz);
+
+    rpy2quaternion.setRPY(rx,ry,rz);
+    rpy2quaternion.normalize();
+    target_pose.orientation = tf2::toMsg(rpy2quaternion);
+    target_pose.position.x = x;
+    target_pose.position.y = y;
+    target_pose.position.z = z;
+    waypoints.push_back(target_pose);
   }
-  inClientFile.close();
+  return;
+}
+
+int main(int argc, char **argv)
+{
+//+-------------------------------------------------------------------------------+
+  ROS_WARN("Iniciando Node");
+  // iniciacao padrao para nodes
+  ros::init(argc, argv, "Reader");
+  ros::NodeHandle node_handle;
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+  /* This sleep is ONLY to allow Rviz to come up */
+  sleep(2.0);
+//+-------------------------------------------------------------------------------+
+  // Obter o endereco relativo do pacote ArmAgeddonRobot
+  string RelativePath=ros::package::getPath("ArmAgeddonRobot");
+  cout << "Caminho Relativo: " << RelativePath << endl;
+//+-------------------------------------------------------------------------------+
+  // Informar o numero de linhas do arquivo 
+  std::ifstream inFile(RelativePath + "/arquivos/positions.txt"); 
+  if(!inFile) {
+    cout << "Arquivo não foi encontrado.\n";
+    return 0;
+  }
+  cout << "Numero de linhas do arquivo: " << std::count(std::istreambuf_iterator<char>(inFile),std::istreambuf_iterator<char>(), '\n')+1 << endl;
+  inFile.close();
+//+-------------------------------------------------------------------------------+
+  // ler o arquivo 
+  ifstream inClientFile(RelativePath + "/arquivos/positions.txt");
   
-  geometry_msgs::Pose target_pose;
   std::vector<geometry_msgs::Pose> waypoints;
-  tf2::Quaternion rpy2quaternion;
 
-
-  rpy2quaternion.setRPY(3.14159, 0, 3.14159);
-  rpy2quaternion.normalize();
-  target_pose.orientation = tf2::toMsg(rpy2quaternion);
-  target_pose.position.x = 0.6836502253486568;
-  target_pose.position.y = 0.6836502253486568;
-  target_pose.position.z = 0.5369882852527241;
+  ExtractPoints(inClientFile,waypoints);
   
-  waypoints.push_back(target_pose);
-  for (auto &valor : waypoints)cout << valor;
+  inClientFile.close();
 
-
+  // for (auto &valor : waypoints)cout << valor;
+//+-------------------------------------------------------------------------------+
+  ROS_WARN("FIM");
+  ros::shutdown();
   return 0;
 }
